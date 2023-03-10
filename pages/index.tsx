dd45bd0f0db72ddabf74007dbf4b9ce43deb261b
Padányi-Gulyás Gergely <gergely.padanyi-gulyas@idomsoft.hu>
2023-03-10 11:26:27 +0100

    initial state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,10 @@
 import styles from "@/styles/Home.module.css";
-import Link from "next/link";
 
 export default function HomePage() {
   return (
     <>
       <div className={styles.container}>
         <h1>The Home Page</h1>
-        <ul>
-          <li>
-            <Link href="/portfolio">Portfolio</Link>
-          </li>
-          <li>
-            <Link href="/clients">Clients</Link>
-          </li>
-        </ul>
       </div>
     </>
   );
