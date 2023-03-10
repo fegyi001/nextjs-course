386c28366a02ee08915e4a4a0ca3efc654c46887
Padányi-Gulyás Gergely <gergely.padanyi-gulyas@idomsoft.hu>
2023-03-10 14:46:30 +0100

    asdfadsfdsfadsf

---
+++ components/layout/layout.tsx
@@ -0,0 +1,13 @@
+import { FC, Fragment, ReactNode } from "react";
+import MainHeader from "@/components/layout/main-header";
+
+const Layout: FC<{ children: ReactNode }> = (props) => {
+  return (
+    <Fragment>
+      <MainHeader />
+      <main>{props.children}</main>
+    </Fragment>
+  );
+};
+
+export default Layout;
